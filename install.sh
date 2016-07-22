5420684e9cfe2e81625059d68565543d57658d01
haohb13 <haohb13@gmail.com>
2016-07-22 21:49:08 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@
 #This is a very simple example
 
 echo Hello World!
+
+cp ./vimrc ~/.vimrc
+
+mkdir -p ~/.vim/bundle/YouCompleteMe/cpp/ycm/
+cp ./.ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/cpp/ycm/
